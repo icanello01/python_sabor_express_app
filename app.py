b1e25890170ca459539669cb3fa0ce642a92cc62
Issame <i_canello@yahoo.com.br>
2025-08-25 23:48:31 -0300

    #

--- app.py
+++ app.py
@@ -6,4 +6,7 @@ print('2. Listar restaurantes')
 
 print('3. Ativar/Desativar restaurante')
 
-print('4. Sair')
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'Você escolheu a opção: {opcao_escolhida}')
