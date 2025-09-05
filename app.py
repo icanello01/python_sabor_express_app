c60b14d6c6cbd7eb2316bcc509ed78473be76b59
Issame <i_canello@yahoo.com.br>
2025-09-04 23:13:44 -0300

    #

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = ["Sushi n' Roll", 'Pizza Place', 'Burger Joint']
+
 def exibir_nome_do_programa():
     print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -20,26 +22,62 @@ def exibir_opcoes():
     print('4. Sair\n')
 
 def finalizar_app():
-    #input('Deseja finalizar o app? (s/n): ')
+    exibir_subtitulo('Finalizando o app...\n')
+
+def voltar_ao_menu_principal():
+    input('\nPressione Enter para voltar ao menu principal...')
+    main()
+
+def opcao_invalida():
+    print('Opção inválida. Tente novamente.\n')
+
+    voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
     os.system('cls' if os.name == 'nt' else 'clear')
-    print('Finalizando o app...\n')
+    print(f'{texto}\n')
+
+def cadastrar_novo_restaurante():
+    exibir_subtitulo('Cadastro de novos restaurantes: ')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante "{nome_do_restaurante}" cadastrado com sucesso!\n')
+
+    voltar_ao_menu_principal()
+
+def listar_restaurantes():
+    exibir_subtitulo('Lista de restaurantes cadastrados:\n')
+    
+    for restaurante in sorted(restaurantes):
+        print(f'- {restaurante}')
+
+    print()
+
+    voltar_ao_menu_principal()
 
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
 
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante:')
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante()
 
-    elif opcao_escolhida == 2:
-        print('Listar restaurantes:')
+        elif opcao_escolhida == 2:
+            listar_restaurantes()
 
-    elif opcao_escolhida == 3:
-        print('Ativar/Desativar restaurante:')
+        elif opcao_escolhida == 3:
+            print('Ativar/Desativar restaurante:')
 
-    else:
-        finalizar_app()
+        elif opcao_escolhida == 4:
+            finalizar_app()
+
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main():
+    os.system('cls' if os.name == 'nt' else 'clear')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
